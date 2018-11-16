38b9d62d190490d6687a9a05cf7d47acfc898752
Cosmin Voicu <cosmin.voicu@gmail.com>
2018-11-16 23:45:11 +0100

    clean up

--- src/Repository/AlbumRepository.php
+++ src/Repository/AlbumRepository.php
@@ -18,33 +18,4 @@ class AlbumRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Album::class);
     }
-
-//    /**
-//     * @return Album[] Returns an array of Album objects
-//     */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Album
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
